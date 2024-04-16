bd07cd7fe18025b88d8ee7d5be464649c6fe1cda
SkhosanaL <lingtonskhosana@gmail.com>
2024-04-16 17:35:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//This is my first Java Script code!  
+var message1 ="First JavaScript Variable";
+var message2 = "Java is Fun";
+var message3 = "Python + CSS + HTML+Java are great Programming languages";
+var message4 = "Generative Artificial Intelligence + ChatGPT."
+       alert (message1);
+       alert (message2);
+       alert (message3);
+       alert (message4);
+    
